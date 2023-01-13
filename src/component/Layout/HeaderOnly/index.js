d30a4c4627a381a262e60b46dc10063572699d2d
Hoc97 <thaihocc.01@gmail.com>
2023-01-13 22:12:53 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/component/Layout/HeaderOnly/index.js
@@ -0,0 +1,15 @@
+import Header from './Header';
+
+function HeaderOnly({ children }) {
+  // console.log(children);
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="content">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderOnly;
